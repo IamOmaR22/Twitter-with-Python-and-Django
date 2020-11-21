3aaabd0e61ed698c1b4ce08d066a0cc38b74ade8
Md. Omar Faruk <iamomar022@gmail.com>
2020-11-22 00:21:54 +0600

    Apps added

--- twitter_django/settings.py
+++ twitter_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '21!k_ho^tvt%ftnj0h5%yqjn+d$@ti0lhe&2mj1x!9mmb1(3r)'
+SECRET_KEY = '21!k_ho^tvt%ftnj0h5%yqjn+domar$@ti0lhe&2mjfaruk1x!9mmb1(3r)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
